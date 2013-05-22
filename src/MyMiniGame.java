217733cd650226a8bab08c6434533772ebde7880
mpfingsten <p83@hotmail.de>
2013-05-22 08:09:40 +0200

    Initial Commit

---
+++ src/MyMiniGame.java
@@ -0,0 +1,76 @@
+import MiniGamePackage.MiniGame;
+
+public class MyMiniGame extends MiniGame 
+{
+
+	@Override
+	protected void computerMove(Action arg0) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void gameHasFinished() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void gameHasStarted() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getCurrentComputerScore() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCurrentPlayerScore() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getName() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNrOfPlayerGoActionsMax(int arg0) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNrofComputerGoActions(int arg0) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void initGame() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void playerMove(Action arg0) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
