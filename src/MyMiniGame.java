69a236aee940818f8996659b191153a93523e0cb
mpfingsten <p83@hotmail.de>
2013-05-22 09:17:25 +0200

    Added KeyDispatcher

--- src/MyMiniGame.java
+++ src/MyMiniGame.java
@@ -1,6 +1,7 @@
 import MiniGamePackage.MiniGame;
 import MiniGamePackage.Sprite;
 
+@SuppressWarnings("serial")
 public class MyMiniGame extends MiniGame
 {
 
@@ -21,146 +22,148 @@ public class MyMiniGame extends MiniGame
 
     public MyMiniGame()
     {
-	getBackgroundPicture().paintRectangle(0, 0, 640, 640, -1, 255, 255, 0);
-
-	fieldSprites[0].paintRectangle(0, 0, 32, 32, -1, 255, 255, 255);
-	fieldSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 0);
-
-	playerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 255, 128, 0);
-	computerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 255);
-
-	computerSprite.paintRectangle(5, 0, 22, 32, -1, 0, 0, 255);
-	playerSprite.paintRectangle(5, 0, 22, 32, -1, 255, 128, 0);
+		getBackgroundPicture().paintRectangle(0, 0, 640, 640, -1, 255, 255, 0);
+	
+		fieldSprites[0].paintRectangle(0, 0, 32, 32, -1, 255, 255, 255);
+		fieldSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 0);
+	
+		playerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 255, 128, 0);
+		computerStoneSprites[0].paintEllipse(5, 5, 22, 22, -1, 0, 0, 255);
+	
+		computerSprite.paintRectangle(5, 0, 22, 32, -1, 0, 0, 255);
+		playerSprite.paintRectangle(5, 0, 22, 32, -1, 255, 128, 0);
 
     }
 
     @Override
     protected void initGame()
     {
-	for (int i = 0; i < NR_OF_FIELDS; i++)
-	{
-	    fieldSprites[i].setPosition(304, i * 40 + 100);
-	    playerStoneSprites[i].setPosition(304, i * 40 + 100);
-	    computerStoneSprites[i].setPosition(304, i * 40 + 100);
-	    playerStoneSprites[i].dontShow();
-	    computerStoneSprites[i].dontShow();
+		for (int i = 0; i < NR_OF_FIELDS; i++)
+		{
+		    fieldSprites[i].setPosition(304, i * 40 + 100);
+		    playerStoneSprites[i].setPosition(304, i * 40 + 100);
+		    computerStoneSprites[i].setPosition(304, i * 40 + 100);
+		    playerStoneSprites[i].dontShow();
+		    computerStoneSprites[i].dontShow();
+	
+		    fieldStatus[i] = 0;
+		}
 
-	    fieldStatus[i] = 0;
-	}
-
-	playerPosition = 0;
-	computerPosition = 0;
-	updatePositions();
+		playerPosition = 0;
+		computerPosition = 0;
+		updatePositions();
     }
 
     @Override
     protected void gameHasStarted()
     {
+    	
     }
 
     @Override
     protected void computerMove(Action action)
     {
-	switch (action)
-	{
-	case DOWN:
-	    computerPosition = Math.min(computerPosition + 1, NR_OF_FIELDS - 1);
-	    updatePositions();
-	    break;
-	case UP:
-	    computerPosition = Math.max(computerPosition - 1, 0);
-	    updatePositions();
-	    break;
-	case GO:
-	    fieldStatus[computerPosition] = 1;
-	    playerStoneSprites[computerPosition].dontShow();
-	    computerStoneSprites[computerPosition].show();
-
-	    break;
-	default:
-	    break;
-	}
+		switch (action)
+		{
+			case DOWN:
+			    computerPosition = Math.min(computerPosition + 1, NR_OF_FIELDS - 1);
+			    updatePositions();
+			    break;
+			case UP:
+			    computerPosition = Math.max(computerPosition - 1, 0);
+			    updatePositions();
+			    break;
+			case GO:
+			    fieldStatus[computerPosition] = 1;
+			    playerStoneSprites[computerPosition].dontShow();
+			    computerStoneSprites[computerPosition].show();
+		
+			    break;
+			default:
+			    break;
+		}
     }
 
     @Override
     protected void playerMove(Action action)
     {
-	switch (action)
-	{
-	case DOWN:
-	    playerPosition = Math.min(playerPosition + 1, NR_OF_FIELDS - 1);
-	    updatePositions();
-	    break;
-	case UP:
-	    playerPosition = Math.max(playerPosition - 1, 0);
-	    updatePositions();
-	    break;
-	case GO:
-	    fieldStatus[playerPosition] = 2;
-	    playerStoneSprites[playerPosition].show();
-	    computerStoneSprites[playerPosition].dontShow();
-
-	    break;
-	default:
-	    break;
-	}
+		switch (action)
+		{
+			case DOWN:
+			    playerPosition = Math.min(playerPosition + 1, NR_OF_FIELDS - 1);
+			    updatePositions();
+			    break;
+			case UP:
+			    playerPosition = Math.max(playerPosition - 1, 0);
+			    updatePositions();
+			    break;
+			case GO:
+			    fieldStatus[playerPosition] = 2;
+			    playerStoneSprites[playerPosition].show();
+			    computerStoneSprites[playerPosition].dontShow();
+		
+			    break;
+			default:
+			    break;
+		}
     }
 
     @Override
     protected void gameHasFinished()
     {
+    	System.out.println("Ende");
     }
 
     @Override
     public int getCurrentComputerScore()
     {
-	int cs = 0;
-	for (int i = 0; i < fieldStatus.length; i++)
-	{
-	    if (fieldStatus[i] == 1)
-	    {
-		++cs;
-	    }
-	}
-	return cs * 100;
+		int cs = 0;
+		for (int i = 0; i < fieldStatus.length; i++)
+		{
+		    if (fieldStatus[i] == 1)
+		    {
+		    	++cs;
+		    }
+		}
+		return cs * 100;
     }
 
     @Override
     public int getCurrentPlayerScore()
     {
-	int ps = 0;
-	for (int i = 0; i < fieldStatus.length; i++)
-	{
-	    if (fieldStatus[i] == 2)
-	    {
-		++ps;
-	    }
-	}
-	return ps * 100;
+		int ps = 0;
+		for (int i = 0; i < fieldStatus.length; i++)
+		{
+		    if (fieldStatus[i] == 2)
+		    {
+		    	++ps;
+		    }
+		}
+		return ps * 100;
     }
 
     @Override
     public String getName()
     {
-	return "OOP Vorlesung #8 Testspiel";
+    	return "OOP Vorlesung #8 Testspiel";
     }
 
     @Override
     public int getNrOfPlayerGoActionsMax(int difficulty)
     {
-	return 10;
+    	return 10;
     }
 
     @Override
     public int getNrofComputerGoActions(int difficulty)
     {
-	return difficulty * 10;
+    	return difficulty * 10;
     }
 
     private void updatePositions()
     {
-	computerSprite.setPosition(200, computerPosition * 40 + 100);
-	playerSprite.setPosition(400, playerPosition * 40 + 100);
+    	computerSprite.setPosition(200, computerPosition * 40 + 100);
+    	playerSprite.setPosition(400, playerPosition * 40 + 100);
     }
 
 }
--- src/MyMiniGameWindow.java
+++ src/MyMiniGameWindow.java
@@ -1,17 +1,17 @@
 import java.awt.BorderLayout;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JToolBar;
-
 import MiniGamePackage.MiniGameObserver;
 
 @SuppressWarnings("serial")
-public class MyMiniGameWindow extends JFrame implements ActionListener, MiniGameObserver
+public class MyMiniGameWindow extends JFrame implements ActionListener, MiniGameObserver, KeyEventDispatcher
 {
-
     JButton buttonUp = new JButton("UP!");
     JButton buttonDown = new JButton("DOWN!");
     JButton buttonGo = new JButton("GO!");
@@ -21,29 +21,30 @@ public class MyMiniGameWindow extends JFrame implements ActionListener, MiniGame
 
     public MyMiniGameWindow()
     {
-	setSize(700, 700);
-
-	setLayout(new BorderLayout());
-
-	JToolBar toolbar = new JToolBar();
-	add(toolbar, BorderLayout.NORTH);
-	toolbar.add(buttonUp);
-	toolbar.add(buttonDown);
-	toolbar.add(buttonGo);
-	toolbar.add(buttonStartGame);
-
-	buttonUp.addActionListener(this);
-	buttonDown.addActionListener(this);
-	buttonGo.addActionListener(this);
-	buttonStartGame.addActionListener(this);
-
-	add(theGame, BorderLayout.CENTER);
-
-	theGame.registerMiniGameObserver(this);
-
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	setVisible(true);
-
+		setSize(700, 700);
+	
+		setLayout(new BorderLayout());
+	
+		JToolBar toolbar = new JToolBar();
+		add(toolbar, BorderLayout.NORTH);
+		//toolbar.add(buttonUp);
+		//toolbar.add(buttonDown);
+		//toolbar.add(buttonGo);
+		toolbar.add(buttonStartGame);
+	
+		buttonUp.addActionListener(this);
+		buttonDown.addActionListener(this);
+		buttonGo.addActionListener(this);
+		buttonStartGame.addActionListener(this);
+	
+		add(theGame, BorderLayout.CENTER);
+	
+		theGame.registerMiniGameObserver(this);
+	
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
     }
 
     @Override
@@ -72,5 +73,34 @@ public class MyMiniGameWindow extends JFrame implements ActionListener, MiniGame
     {
 	setTitle("Time left: " + timeLeft + " Player: " + playerScore + "  Computer: " + computerScore + " Still running:" + isRunning);
     }
+
+	@Override
+	public boolean dispatchKeyEvent(KeyEvent ke)
+	{
+		System.out.println(ke.getKeyChar());
+		if (ke.getID() == KeyEvent.KEY_PRESSED)
+		{
+			switch (ke.getKeyCode())
+			{
+				case KeyEvent.VK_SPACE:
+					theGame.playerActionGo();
+					return true;
+					
+				case KeyEvent.VK_DOWN:
+					theGame.playerActionDown();
+					return true;
+					
+				case KeyEvent.VK_UP:
+					theGame.playerActionUp();
+					return true;
+					
+				case KeyEvent.VK_F11:
+					System.out.println("F11");
+					theGame.newGame(1);
+					return true;
+			}			
+		}
+		return true;
+	}
 
 }
